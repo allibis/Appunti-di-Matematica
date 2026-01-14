9fafcce095ef0e66e9febbd1597e82cff708faea
allibis <andrix1250@gmail.com>
2026-01-14 21:26:23 +0100

    Quartz sync: Jan 14, 2026, 9:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "it-IT",
     baseUrl: "allibis.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian", ".excalidraw", "Dashboard"],
+    ignorePatterns: ["private", "templates", ".obsidian", ".excalidraw", "Dashboard.md"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
