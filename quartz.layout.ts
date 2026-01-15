114b16b285e3a54f30a599316b87c3154bd68476
allibis <andrix1250@gmail.com>
2026-01-16 00:20:41 +0100

    Quartz sync: Jan 16, 2026, 12:20 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -22,8 +22,12 @@ export const defaultContentPageLayout: PageLayout = {
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
-    
+    Component.ConditionalRender({
+      component: Component.TagList(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     // Component.ContentMeta(),
+    
     Component.TagList(),
   ],
   left: [
@@ -73,7 +77,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
